89a3140a4dd0862837ad4e5e2ac3a92a1e6105ec
Mark Sz <enchronin@gmail.com>
2025-07-17 17:22:02 +0200

    upload sql files

---
+++ project_sql/2_top_paying_skills.sql
@@ -0,0 +1,117 @@
+/*
+Question: What skills are required for the top-paying data analyst jobs?
+- Use the top 10 highest-paying Data Analyst jobs from first query
+- Add the specific skills required for these roles
+- Why? It provides a detailed look at which high-paying jobs demand certain skills, 
+    helping job seekers understand which skills to develop that align with top salaries
+*/
+
+WITH top_paying_jobs AS(
+
+SELECT
+    job_id,
+    job_title,
+    salary_year_avg,
+    c.name AS company_name
+    FROM 
+        job_postings_fact AS j
+LEFT JOIN company_dim AS c ON 
+j.company_id = c.company_id
+
+WHERE
+    job_title_short = 'Data Analyst' 
+    AND job_location = 'Anywhere'
+    AND salary_year_avg iS NOT NULL
+ORDER BY 
+    salary_year_avg DESC
+LIMIT 10
+)
+
+SELECT 
+top_paying_jobs.*,
+skills
+FROM top_paying_jobs
+INNER JOIN skills_job_dim AS s      
+ON s.job_id = top_paying_jobs.job_id
+INNER JOIN skills_dim
+ON skills_dim.skill_id = s.skill_id
+ORDER BY salary_year_avg DESC
+LIMIT 10;
+
+
+/*
+
+[
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "sql"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "python"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "r"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "azure"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "databricks"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "aws"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "pandas"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "pyspark"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "jupyter"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "salary_year_avg": "255829.5",
+    "company_name": "AT&T",
+    "skills": "excel"
+  }
+]
+
+*/
